739fa4539f3a589fee38db16aa8016a114f40eec
Danilo Bargen <mail@dbrgn.ch>
2017-12-30 00:50:35 +0100

    Re-export pest

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 #[cfg(not(test))]
-extern crate pest;
+pub extern crate pest;
 #[cfg(test)]
 #[macro_use]
-extern crate pest;
+pub extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
@@ -15,5 +15,21 @@ const _GRAMMAR: &'static str = include_str!("../fontobene.pest");
 #[derive(Parser)]
 #[grammar = "../fontobene.pest"]
 /// The main parser.
+///
+/// Use it like this:
+///
+/// ```should_panic
+/// extern crate fontobene;
+///
+/// use fontobene::{FontobeneParser, Rule};
+/// use fontobene::pest::{Parser};
+///
+/// fn main() {
+///     let input = "..."; // The font file contents to parse
+///
+///     let _parsed = FontobeneParser::parse_str(Rule::main, &input)
+///         .unwrap_or_else(|e| panic!("{}", e));
+/// }
+/// ```
 
 pub struct FontobeneParser;
