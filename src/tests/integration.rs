1a69fb819818217bbb450169676b2bd11dd924d7
Danilo Bargen <mail@dbrgn.ch>
2018-04-17 23:09:03 +0200

    Add fontobene-fonts for testing

--- src/tests/integration.rs
+++ src/tests/integration.rs
@@ -2,11 +2,11 @@ use pest::Parser;
 
 use ::{FontobeneParser, Rule};
 
-static FONT_STANDARD: &'static str = include_str!("fonts/standard.bene");
+static FONT_NEWSTROKE: &'static str = include_str!("fonts/newstroke.bene");
 
 /// The parser should be able to parse an entire file without errors.
 #[test]
-fn parse_standard() {
-    FontobeneParser::parse_str(Rule::main, &FONT_STANDARD)
+fn parse_newstroke() {
+    FontobeneParser::parse_str(Rule::main, &FONT_NEWSTROKE)
         .unwrap_or_else(|e| panic!("{}", e));
 }
